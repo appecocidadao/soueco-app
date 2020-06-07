3f11fcf35cfed205fbc02a1175061b07674b3734
IFULL <ifull@MacBook-Pro.lan>
2020-06-06 22:45:49 -0400

    update master

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const api = axios.create({
   // baseURL: 'http://10.0.2.2:3333',
-  baseURL: 'http://206.189.194.249',
+  // baseURL: 'http://206.189.194.249',
+  baseURL: 'https://soueco-api.tce.am.gov.br',
 });
 
 export default api;
